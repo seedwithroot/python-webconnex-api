620f3d29ccec1efb7470a349949f0a5b5cd95115
Justin <seedwithroot@gmail.com>
2014-04-02 05:56:48 -0700

    registrant

--- pyWebconnex.py
+++ pyWebconnex.py
@@ -5,7 +5,7 @@
 
 '''A library that provides a Python interface to the Webconnex REST API'''
 
-__author__ = 'python-twitter@googlegroups.com'
+__author__ = 'seedwithroot@gmail.com'
 __version__ = '0.8.5'
 
 
@@ -23,12 +23,12 @@ class WebconnexError(Exception):
 
 class API(object):
     def __init__(self, api_key=None,base_url=None):
-    
+
         if base_url is None:
             self.base_url = 'https://cp.webconnex.com/api'
         else:
             self.base_url = base_url
-        
+
         self.api_key = api_key
 
     def GetRegistrations(self):
@@ -41,16 +41,20 @@ class API(object):
             r = Registrant(id=i['id'],first_name=i['first_name'],last_name=i['last_name'],status=i['status'],amount=i['amount'])
             registrants.append(r)
         return registrants
-            
-        
-    
+
+    def _GetRegistrantDetailsRaw(self,registrant_id):
+        url  = '%s/%s/registrations/%d' % (self.base_url,self.api_key,registrant_id)
+        json = self._FetchUrl(url)
+        data = self._ParseAndCheck(json)
+        return data
+
     def _CheckForWebconnexError(self, data):
         """Raises a TwitterError if twitter returns an error message.
-    
+
         Args:
           data:
             A python dict created from the Twitter json response
-    
+
         Raises:
           TwitterError wrapping the twitter error message if one exists.
         """
@@ -67,7 +71,7 @@ class API(object):
         else:
             response = requests.get(url,headers=headers)
         return response.json()
-        
+
     def _ParseAndCheck(self, json):
         """Try and parse the JSON returned from Twitter and return
         an empty dictionary if there is any error. This is a purely
@@ -82,21 +86,21 @@ class API(object):
           if "<title>Webconnex / Error</title>" in json:
             raise TwitterError("Technical Error")
           raise TwitterError("json decoding")
-    
+
         return data
 
 class Registrant(object):
-    
+
     def __init__(self,id=None,first_name=None,last_name=None,status=None,amount=None):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.status = status
         self.amount = amount
-    
+
     def GetFirstName(self):
         '''Get the time this status message was posted.
-        
+
         Returns:
         The time this status message was posted
         '''
@@ -104,14 +108,11 @@ class Registrant(object):
 
     def SetFirstName(self, first_name):
         '''Set the time this status message was posted.
-        
+
         Args:
           created_at:
             The time this status message was created
         '''
         self._first_name = first_name
-    
+
     first_name = property(GetFirstName, SetFirstName,doc='The time this status message was posted.')
-    
-    
-        
